db39a00e44e08389af241f4537782b8a9b32b386
jentlapalm <81242269+jentlapalm@users.noreply.github.com>
2021-11-04 21:21:51 -0400

    Add files via upload

--- EE#2.R
+++ EE#2.R
@@ -7,11 +7,14 @@ library(Matrix)
 library(expm)
 library(tidyverse)
 library(stringr)
+library(lubridate)
+library(ggplot2)
+library(ggthemes)
 
 # home wd
-# setwd("C:\\Users\\Jent\\Documents\\College\\Emory\\Social Network Analytics\\EE#2")
+setwd("C:\\Users\\Jent\\Documents\\College\\Emory\\Social Network Analytics\\EE#2")
 # school wd
-setwd("C:\\Users\\jentl\\Documents\\Emory\\Fall 2021\\Social Network Analytics\\EE#2")
+# setwd("C:\\Users\\jentl\\Documents\\Emory\\Fall 2021\\Social Network Analytics\\EE#2")
 
 f1 <- fread("Funding_events_7.14.csv")
 f2 <- read_excel("Funding_events_7.14_page2.xlsx")
@@ -24,8 +27,6 @@ f1dt <- data.table(date=f1$`Deal Date`,investors=f1$Investors)
 f2dt <- data.table(date=f2$`Deal Date`,investors=f2$Investors)
 
 # converting to datetime
-library(lubridate)
-
 f1dt$date <- mdy(f1dt$date)
 f2dt$date <- ymd(f2dt$date)
 
@@ -34,8 +35,6 @@ dt <- rbind(f1dt,f2dt)
 
 # remove NA rows
 dt <- na.omit(dt)
-
-# separate investors into multiple columns
 
 #checking for the max number of investors
 ncols <- max(stringr::str_count(dt$investors, ",")) + 1 # 27 investors
@@ -73,14 +72,13 @@ for (i in 1:nrow(dtwide)){
 # remove repeat rows
 network <- unique(network)
 
-#need to remove rows with only Inc.
-inc <- network[224296,2]
-network <- network[!investor_1==inc]
-network <- network[!investor_2==inc]
-
 # removing whitespace
 network$investor_1 <- trimws(network$investor_1, which = c("left"))
 network$investor_2 <- trimws(network$investor_2, which = c("left"))
+
+#need to remove rows with only Inc.
+network <- network[!investor_1=='Inc.']
+network <- network[!investor_2=='Inc.']
 
 # decaying old ties
 # first, get all renewal windows for all ties in network
@@ -107,6 +105,65 @@ bacon <- unique(network[date<"2014-08-01",2:3])
 bgraph <- graph_from_data_frame(bacon, directed=F)
 
 most_central <- sort(closeness(bgraph), decreasing=T) 
-# highest is Intel Capital with closeness of 7.946447e-07
+
+most_central[1] # highest is Intel Capital with closeness of 7.946447e-07
+
+"QUESTION 2
+Allow the network to be up dated monthly for each month in the data, adding the new ties that occur through investments in the current month to be added to the existing network of ties that have occurred in previous months.
+
+Plot the average k-core of each venture capital firm in the network over time. This can be computed using the igraph function coreness. On the x-axis should be time. On the y-axis should be the highest-degree k-core each venture capital firm belongs to, averaged over all firms in the network up to that month."
+
+# x and y vectors
+
+x <- lubridate::ymd()
+y <- c()
+
+# loop to calculate x and y
+
+for (i in 1:375){
+  # subset network
+  subnet <- network[date %between% .(date[1],date[1]+months(i)),2:3]
+  # add end-date to x
+  x <- c(x,network$date[1]+months(i))
+  # convert to igraph, calculate coreness
+  core <- coreness(graph_from_data_frame(subnet))
+  # max over average
+  avg_k_core <- max(core)/mean(core)
+  # save to y
+  y <- c(y,avg_k_core)
+}
+
+#plotting with ggplot
+ggdt <- data.table(x,y)
+
+ggplot(ggdt,aes(x,y))+geom_line()+theme_wsj()+theme(axis.title = element_text())+xlab('Time')+ylab('Average k-core')+ggtitle('Investor Avg k-core Over Time')
+
+"QUESTION #3 (A)
+Next, we will look at the development of the venture capital firm co-investment network in
+terms of its global core-periphery structure. 
+
+Allow the network to be updated monthly, as in Question 2.
+
+Calculate the co-investment network's concentration scores to determine if it tends towards a core-periphery structure across the data. 
+
+Illustrate a figure, with one plot for one month from each calendar year in the data, 
+that shows the range of concentration scores for each partition size p for the range of 
+p in 1 to the number of nodes in the networks in the network for that month's cross-section. 
+
+You can exclude the very early period of the data when all of the firms have the same eigenvector centrality."
+
+# allie's advice:
+"it is taking the centrality of each month of the data and sorting it then creating [1 ..0] matrices and computing the correlation with each of those matrices where you add a 1 each time"
+
+# test case before loop
+# first, choose a month. get subnet up to that month. get number of nodes/vertices (x axis). get range(concentration scores) for each partition size
+
+avg_date <- mean(network$date)
+test <- network[date<=avg_date,2:3]
+testgraph <- graph_from_data_frame(test)
+num_nodes <- length(V(testgraph))
 
 
+
+"QUESTION #3(B)
+Provide one other piece of descriptive evidence outside of the concentration scores to support your conclusion. You can see Slide 29 of Class 4 for some examples of evidence to use."
